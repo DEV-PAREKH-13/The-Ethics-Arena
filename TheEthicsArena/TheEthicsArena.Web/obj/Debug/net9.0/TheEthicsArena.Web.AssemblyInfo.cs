78ce12211d295be71e4ff26728bddefbce990446
DEV <Lucifer131313@users.noreply.github.com>
2025-09-16 18:31:00 +0530

    Add complete navigation layout with dilemma dropdown - complete #3

--- TheEthicsArena/TheEthicsArena.Web/obj/Debug/net9.0/TheEthicsArena.Web.AssemblyInfo.cs
+++ TheEthicsArena/TheEthicsArena.Web/obj/Debug/net9.0/TheEthicsArena.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TheEthicsArena.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4690098759d327858293f3e15f0edc1338546e4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbfec96ce76aa6b31ab294396113a509bd4bab1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TheEthicsArena.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TheEthicsArena.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
